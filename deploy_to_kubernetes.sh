1c2259d79db45b8616220a6a8975dcf9d1a6cb59
Pal David Gergely <pal.gergely@doctusoft.com>
2018-03-16 15:48:38 +0100

    Made the Docker image version tag unique in the deploy_to_kubernetes.sh to make sure that it will work when deploying to Kubernetes. Also made sure that it works on multiple platforms

--- deploy_to_kubernetes.sh
+++ deploy_to_kubernetes.sh
@@ -2,10 +2,23 @@
 
 set -euf -o pipefail
 
-PROJECT="$(gcloud config configurations list --filter is_active:true --format 'value(properties.core.project)')"
+function do_version_str_with_python {
+  NEW_VERSION_STR="$(python -c 'from datetime import datetime; print datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f")')"
+}
 
+PROJECT="$(gcloud config configurations list --filter is_active:true --format 'value(properties.core.project)')"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # OSX date command does not support showing milliseconds in any way, so using Python from the OSX default install
+  do_version_str_with_python
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+  # FreeBSD also does have the non-GNU date command and usually have Python installed, so do the same as with OSX
+  do_version_str_with_python
+else
+  # On every other OS, date command with "%N" in the format string should work, because they hopefully use GNU date
+  NEW_VERSION_STR="$(date '+%Y-%m-%d-%H-%M-%S-%6N')"
+fi
 
 kubectl delete deployment serverless-imager &> /dev/null || true
-docker build -t gcr.io/$PROJECT/serverless-imager:v1 .
-gcloud docker -- push gcr.io/$PROJECT/serverless-imager:v1
-kubectl run serverless-imager --image=gcr.io/$PROJECT/serverless-imager:v1 --port 8080
+docker build -t "gcr.io/${PROJECT}/serverless-imager:${NEW_VERSION_STR}" .
+gcloud docker -- push "gcr.io/${PROJECT}/serverless-imager:${NEW_VERSION_STR}"
+kubectl run serverless-imager --image="gcr.io/${PROJECT}/serverless-imager:${NEW_VERSION_STR}" --port 8080
